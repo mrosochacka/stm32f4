7767798d5de054a185e87d6741ce9b7e5460c9cd
mrosochacka <magdalena.rosochacka94@gmail.com>
2016-02-05 00:35:05 +0100

    wysyłanie napisu

--- src/main.c
+++ src/main.c
@@ -84,12 +84,12 @@ void GPIO_Config(void)
 
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART1);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_USART1);
 
 	//PC6 - Tx PC7 - Rx
 	GPIO_InitTypeDef  GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -207,7 +207,24 @@ void ADC_Config() {
 	ADC_Cmd(ADC1, ENABLE);
 
 }
+void USART_Config(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 
+	USART_InitTypeDef  USART_InitStructure;
+
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
+
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
 void USART_puts(volatile char *s){
 
 while(*s){
